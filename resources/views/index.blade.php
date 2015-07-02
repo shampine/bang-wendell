37df8facd1db843bf30b8f91951459078af40299
patrickshampine <patrick@patrickshampine.com>
2015-07-01 23:15:06 -0700

    Added a logo, centered stuff

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,10 +11,17 @@
     <link rel="stylesheet" href="/css/main.css">
   </head>
   <body>
-    <section class="content">
-      <h1>Currently Wendell is {{{ $status ? 'open' : 'closed' }}}</h1>
-      <h2>{{{ date('h:m:Y') }}}</h2>
-      <h3>{{{ $_SERVER['REQUEST_TIME'] }}}</h3>
+    <section class="wrapper">
+      <header>
+        <h1><img src="/img/wendell.png" alt="Wendell Bar - Dowtown Los Angeles 90014"></h1>
+      </header>
+      <article>
+        <span class="title">Currently Wendell is</span>
+        <h1>{{{ $status ? 'open' : 'closed' }}}</h1>
+      </article>
+      <footer>
+        <p>{{{ date('h:m A') }}}</p>
+      </footer>
     </section>
     <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
     <script src="/js/main.js"></script>
