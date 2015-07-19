165d96045d010e08496c080b912241006786da87
patrickshampine <patrick@patrickshampine.com>
2015-07-18 23:01:19 -0700

    Indexing enabled

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,10 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="robots" content="noindex, nofollow">
+    <meta name="robots" content="index, follow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <meta name="description" content="Is Wendell Bar in downtown Los Angeles open? Find out here or update their status.">
-    <title>Is Wendell Open</title>
+    <meta name="description" content="Is Wendell Bar (90014) in downtown Los Angeles open? Find out here or update their status.">
+    <title>Is Wendell Open | DTLA 90014</title>
     <link rel="shortcut icon" href="/img/favicon.ico">
     <link rel="stylesheet" href="/css/main.css">
     <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
